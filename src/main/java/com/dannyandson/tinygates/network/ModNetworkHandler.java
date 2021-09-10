d4ef505b5e6dd69efbed746424240cc0a29d67ff
danny <dannydjdk@users.noreply.github.com>
2021-09-10 14:36:17 -0500

    -Initial backport to 1.16.5 -Added SHIFT activated item documentation similar to Tiny Redstone -Tiny Clock now syncs directly to client to allow animation to update when output doesn't change.

--- src/main/java/com/dannyandson/tinygates/network/ModNetworkHandler.java
+++ src/main/java/com/dannyandson/tinygates/network/ModNetworkHandler.java
@@ -1,8 +1,12 @@
 package com.dannyandson.tinygates.network;
 
 import com.dannyandson.tinygates.TinyGates;
+import com.dannyandson.tinyredstone.blocks.PanelTile;
+import com.dannyandson.tinyredstone.network.PanelCellSync;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
@@ -28,10 +32,21 @@ public class ModNetworkHandler {
                 .decoder(ClockTickSync::new)
                 .consumer(ClockTickSync::handle)
                 .add();
+
+        INSTANCE.messageBuilder(PanelCellSync.class,nextID())
+                .encoder(PanelCellSync::toBytes)
+                .decoder(PanelCellSync::new)
+                .consumer(PanelCellSync::handle)
+                .add();
     }
 
-    public static void sendToClient(Object packet, ServerPlayerEntity player) {
-        INSTANCE.sendTo(packet, player.connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+    public static void sendToClient(Object packet, PanelTile panelTile) {
+        BlockPos pos = panelTile.getBlockPos();
+        for (PlayerEntity player : panelTile.getLevel().players()) {
+            if (player instanceof ServerPlayerEntity && player.distanceToSqr((double) pos.getX(), (double) pos.getY(), (double) pos.getZ()) < 64d) {
+                INSTANCE.sendTo(packet, ((ServerPlayerEntity) player).connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+            }
+        }
     }
 
     public static void sendToServer(Object packet) {
